346a9ad95594d6127d27ba9a6cd5dbed7ee78b8f
juniorzhp <juniorzhp@gmail.com>
2013-09-01 23:03:55 +0800

    first push

---
+++ components/album_list/index.js
@@ -0,0 +1,18 @@
+KISSY.add('components/album_list/index', function(S, Brick){
+	var $ = S.all;
+
+	var View = Brick.extend({
+		bindUI : function(){
+			
+		}
+	},{
+    	ATTRS : {
+            
+        },
+        EVENTS : {
+        	
+        }
+    });
+
+	return View;
+},{requires:['brix/core/brick']})
