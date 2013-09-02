df9aa84d49d7d4f0e40a67ef2ea9ba7ac41cfc9b
juniorzhp <juniorzhp@gmail.com>
2013-09-02 23:00:17 +0800

    update

--- components/album_list/index.js
+++ components/album_list/index.js
@@ -1,23 +1,28 @@
-KISSY.add('components/album_list/index', function(S, Brick){
-	var $ = S.all;
+ KISSY.add('components/album_list/index', function(S, Brick ,Helper){
+    var $ = S.all;
 
-	var View = Brick.extend({
-		bindUI : function(){
-			
-		}
-	},{
-    	ATTRS : {
+    var View = Brick.extend({
+        bindUI : function(){
+            var self = this;
+
+        }
+
+    },{
+        ATTRS : {
             
         },
         EVENTS : {
-        	'#J_list li' : {
-        		click : function(e){
-        			var curNode = $(e.currentTarget);
-        			
-        		}
-        	}
+            '#J_list li' : {
+                click : function(e){
+                    var curNode = $(e.currentTarget);
+                    curNode.animate({opacity:0}, 1, 'backOut', function(){
+                        curNode.remove();
+                    });
+                }
+            }
         }
     });
 
-	return View;
-},{requires:['brix/core/brick']})
+    return View;
+
+ },{requires:['brix/core/brick','components/helper/']});
